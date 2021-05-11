138028c969324d2bb8a60a3e21b68fb8e4a51c9f
Michael Hutchings <michael.hutchings@cambridgeconsultants.com>
2021-05-11 15:30:56 +0100

    remove nextjs Container (is deprecated)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import "../styles/globals.css"
-import { Container } from "next/app"
 import Head from "next/head"
 import { ChakraProvider } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
     return (
         <ChakraProvider>
-            <Container>
+            <>
                 <Head>
                     <link
                         href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
@@ -14,7 +13,7 @@ function MyApp({ Component, pageProps }) {
                     />
                 </Head>
                 <Component {...pageProps} />
-            </Container>
+            </>
         </ChakraProvider>
     )
 }
